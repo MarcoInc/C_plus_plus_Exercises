48a1e8ff990f25be3c36250ea72bf588e024d7a7
MarcoInc <soteramarco@gmail.com>
2023-04-15 12:52:32 +0200

    fix name

--- Liste/1. Lista.cpp
+++ Liste/1. Lista.cpp
@@ -11,17 +11,17 @@ CODA E' L'ULTIMO NODO E NON PUNTA A NIENTE
 
 TESTA	 NODO	   CODA
 |5,*| -> |2,*| -> |20,*| -> null
- I°	      II°	   III°		 
+ Iï¿½	      IIï¿½	   IIIï¿½		 
   
 LA TESTA CHE CONTIENE 5, PUNTA AL SECONDO ELEMENTO CHE CONTIENE 2.
 IL SECONDO ELEMENTO CHE CONTIENE 2 PUNTA AL TERZO ELEMENTO CHE CONTIENE 20.
 IL TERZO ELEMENTO CHE CONTIENE 20 PUNTA A NIENTE QUINDI E' UNA CODA.
 
 SE AGGIUNGO UN NUOVO NODO, QUESTO SARA' UNA NUOVA CODA
-
+ 
 TESTA	 NODO	   NODO		 CODA
 |5,*| -> |2,*| -> |20,*| -> |12,*| -> null
- I°	      II°	   III°		 IV°
+ Iï¿½	      IIï¿½	   IIIï¿½		 IVï¿½
 
 */
 #include <iostream>				//Gestisce stream Input e Output -> fa funzionare il COUT e CIN
@@ -37,7 +37,7 @@ int main(){
 	nodo* testa=new nodo;	//creo un nodo usato come testa della lista
 	//scrivo l'informazione nel nodo testa
 	testa->num=5;			//PUNTATORE->NOME VARIABILE , si usa per accedere ad un puntatore dentro una struttura
-	testa->succ=0;			//per ora punta a 0, perchè non c'è un elemento successivo
+	testa->succ=0;			//per ora punta a 0, perchï¿½ non c'ï¿½ un elemento successivo
 	nodo* nuovo=new nodo;		//creazione nodo
 
 	cout<<"Inserisci il numero da inserire nel nodo \n";
